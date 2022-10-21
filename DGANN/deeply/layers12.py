23962f6ef0d68935af2297c59bb013b0ed5eb9a6
Silence-ctrl <ljw1587745027@163.com>
2022-10-21 22:49:27 +0800

    first commit

---
+++ DGANN/deeply/layers12.py
@@ -0,0 +1,140 @@
+import torch
+import torch.nn.functional as F
+from torch.nn.modules.module import Module
+from torch.nn.parameter import Parameter
+import torch.nn as nn
+
+class GraphConvolution(Module):
+    """
+    Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+    """
+
+    def __init__(self, in_features, out_features, dropout=0., act=F.relu):
+        super(GraphConvolution, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.dropout = dropout
+        self.act = act
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.weight)
+
+    def forward(self, input, adj):
+        input = F.dropout(input, self.dropout, self.training)
+        support = torch.mm(input, self.weight)
+        output = torch.spmm(adj, support)
+        output = self.act(output)
+        return output
+
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
+
+class GraphAttentionLayer(Module):
+    """
+    Simple GAT layer, similar to https://arxiv.org/abs/1710.10903 
+    图注意力层
+    """
+    def __init__(self, in_features, out_features, alpha=0.2, dropout=0.2, concat=True):
+        super(GraphAttentionLayer, self).__init__()
+        self.in_features = in_features   # 节点表示向量的输入特征维度
+        self.out_features = out_features   # 节点表示向量的输出特征维度
+        self.dropout = dropout    # dropout参数
+
+        self.alpha = alpha     # leakyrelu激活的参数
+   
+        self.concat = concat   # 如果为true, 再进行elu激活
+        
+        # 定义可训练参数，即论文中的W和a
+        self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))  
+        nn.init.xavier_uniform_(self.W.data, gain=1.414)  # xavier初始化
+        self.a = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
+        nn.init.xavier_uniform_(self.a.data, gain=1.414)   # xavier初始化
+        
+        self.W_2 = nn.Parameter(torch.zeros(size=(in_features, out_features)))  
+        nn.init.xavier_uniform_(self.W_2.data, gain=1.414)  # xavier初始化
+        
+        self.W_3 = nn.Parameter(torch.zeros(size=(in_features, out_features)))  
+        nn.init.xavier_uniform_(self.W_3.data, gain=1.414)  # xavier初始化
+        # self.a_2 = nn.Parameter(torch.zeros(size=(2*out_features, 1)))
+        # nn.init.xavier_uniform_(self.a_2.data, gain=1.414)   # xavier初始化
+        
+        # 定义leakyrelu激活函数
+        self.leakyrelu = nn.LeakyReLU(self.alpha)
+        # self.leakyrelu2 = nn.LeakyReLU(self.alpha)
+    
+    def forward(self, inp, adj):
+        """
+        inp: input_fea [N, in_features]  in_features表示节点的输入特征向量元素个数
+        adj: 图的邻接矩阵 维度[N, N] 非零即一，数据结构基本知识
+        """
+        # h = torch.mm(inp, self.W)   # [N, out_features]
+        # # h_2 = torch.mm(inp, self.W_2)   # [N, out_features]
+        # N = h.size()[0]    # N 图的节点数
+        
+        # # a_input = torch.cat([h.repeat(1, N).view(N*N, -1), h_2.repeat(N, 1)], dim=1).view(N, -1, 2*self.out_features)
+        # # [N, N, 2*out_features]
+        # e = self.leakyrelu(torch.matmul(a_input, self.a).squeeze(2))
+        # # [N, N, 1] => [N, N] 图注意力的相关系数（未归一化）
+    
+        # zero_vec = -1e12 * torch.ones_like(e)    # 将没有连接的边置为负无穷
+        # attention = torch.where(adj>0, e, zero_vec)   # [N, N]
+       
+        # attention = F.softmax(attention, dim=1)    # softmax形状保持不变 [N, N]，得到归一化的注意力权重！
+        
+        # attention = F.dropout(attention, self.dropout, training=self.training)   # dropout，防止过拟合
+        # h_prime = torch.matmul(attention, h)  # [N, N].[N, out_features] => [N, out_features]
+        # # 得到由周围节点通过注意力权重进行更新的表示
+        
+        #GAT Diffusion step two
+        # a_input_2 = torch.cat([h_2.repeat(1, N).view(N*N, -1), h_2.repeat(N, 1)], dim=1).view(N, -1, 2*self.out_features)
+        # # [N, N, 2*out_features]
+        # e_2 = self.leakyrelu2(torch.matmul(a_input_2, self.a_2).squeeze(2))
+        # # [N, N, 1] => [N, N] 图注意力的相关系数（未归一化）
+        
+        # zero_vec_2 = -1e12 * torch.ones_like(e_2)    # 将没有连接的边置为负_2无穷
+        # attention_2 = torch.where(adj*adj>0, e_2, zero_vec_2)   # [N, N]
+        # # 表示如果邻接矩阵元素大于0时，则两个节点有连接，该位置的注意力系数保留，
+        # # 否则需要mask并置为非常小的值，原因是softmax的时候这个最小值会不考虑。
+        # attention_2 = F.softmax(attention_2, dim=1)    # softmax形状保持不变 [N, N]，得到归一化的注意力权重！
+        # attention_2 = F.dropout(attention_2, self.dropout, training=self.training)   # dropout，防止过拟合
+        # h_prime_2 = torch.matmul(attention_2, h_2)  # [N, N].[N, out_features] => [N, out_features]
+        # 得到由周围节点通过注意力权重进行更新的表示
+        
+        ''' ## Message Passing network
+        # a_input_3 = torch.cat([h_prime.repeat(1, N).view(N*N, -1), h_prime.repeat(N, 1)], dim=1).view(N, -1, 2 *self.out_features)
+        # e_3 = self.leakyrelu(torch.matmul(a_input_3, self.a_2).squeeze(2))
+        # zero_vec_3 = -1e12 * torch.ones_like(e_3)
+        # attention_3 = torch.where(adj>0, e_3, zero_vec_3)
+        # attention_3 = torch.softmax(attention_3, dim=1)
+        # attention_3 = F.dropout(attention_3, self.dropout, training=self.training)
+        # h_prime_3 = torch.matmul(attention_3, h_prime) '''
+        
+        
+        #### grap former
+        
+        h = torch.mm(inp, self.W)
+        h_2 = torch.mm(inp, self.W_2)
+        
+        e = self.leakyrelu(torch.mm(h, h_2.t()))
+        zero_vec = -1e12 * torch.ones_like(e)
+        attention = torch.where(adj>0, e, zero_vec)
+        attention = F.softmax(attention, dim=1)
+        attention = F.dropout(attention, self.dropout, training=self.training)
+        
+        # h_3 = torch.mm(inp, self.W_3)
+        # h_prime = torch.mm(attention, h_3)
+    
+        h_prime = torch.mm((torch.mm(attention, inp) + inp), self.W_3)
+          
+        if self.concat:
+            return F.elu(h_prime)
+        else:
+            return h_prime
+    def __repr__(self):
+        return self.__class__.__name__ + ' (' \
+               + str(self.in_features) + ' -> ' \
+               + str(self.out_features) + ')'
